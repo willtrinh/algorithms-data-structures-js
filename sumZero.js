14184d365bc4c91edd3fd0e891b6725c6f75e2dc
willtrinh <williamltrinh@gmail.com>
2021-06-30 13:24:33 -0700

    Added naive solution for sumZero

--- sumZero.js
+++ sumZero.js
@@ -4,6 +4,18 @@
  * or undefined if a pair does not exist.
  */
 
+// naive solution, nested loops
+function sumZeroNaive(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+// optimized solution, O(n) time complexity
 function sumZero(arr) {
   let left = 0;
   let right = arr.length - 1;
@@ -12,16 +24,16 @@ function sumZero(arr) {
   if (arr[left] > 0) return undefined;
 
   while (left < right) {
-    if (arr[right] + arr[left] === 0) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
       return [arr[left], arr[right]];
       // if sum is less than 0, move left pointer forward
-    } else if (arr[right] + arr[left] < 0) {
+    } else if (sum < 0) {
       left++;
     } else {
       right--;
     }
   }
-  return undefined;
 }
 
 // test cases
