18e1bb32e55948db00cc03e4654d8caa8e35ff0c
willtrinh <williamltrinh@gmail.com>
2021-07-07 09:55:53 -0700

    Implemented factorial & power recursion

---
+++ Recursion/factorial.js
@@ -0,0 +1,16 @@
+// iterative
+// function factorial(num) {
+//   let total = 1;
+//   for (let i = num; i > 1; i--) {
+//     total *= i;
+//   }
+//   return total;
+// }
+
+// recursive
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(5));
